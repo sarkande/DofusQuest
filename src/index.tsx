72c703c6f13dda59f12598b00b96a3b408eabc4c
AllanPure <aperezgo74@gmail.com>
2024-07-21 01:10:09 +0200

    Add bootstrap/react-bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,32 @@
-  import React from 'react';
-  import ReactDOM from 'react-dom/client';
-  import '@styles/pages/index.scss';
-  import reportWebVitals from './reportWebVitals';
-  import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-  import '@styles/pages/index.scss';
-  import Hub from './pages/Hub/Hub';
-  import Settings from 'pages/Settings/Settings';
-  import NotFound from 'pages/Error/NotFound';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@styles/pages/index.scss";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "@styles/pages/index.scss";
+import Hub from "./pages/Hub/Hub";
+import Settings from "pages/Settings/Settings";
+import NotFound from "pages/Error/NotFound";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-  const root = ReactDOM.createRoot(document.getElementById('root')!);
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Hub />} />
-          <Route path="/hub" element={<Hub />} />
-          <Route path="/dofus-quest" element={<NotFound />} />
-          <Route path="/better-naio" element={<NotFound />} />
-          <Route path="/settings" element={<Settings />} />
-          {/* Ajoutez d'autres routes ici */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </React.StrictMode>
-  );
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hub />} />
+        <Route path="/hub" element={<Hub />} />
+        <Route path="/dofus-quest" element={<NotFound />} />
+        <Route path="/better-naio" element={<NotFound />} />
+        <Route path="/settings" element={<Settings />} />
+        {/* Ajoutez d'autres routes ici */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
 
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,11 +1,24 @@
-import '@styles/pages/Settings.scss';
-import { NavLink } from 'react-router-dom';
+import "@styles/pages/Settings.scss";
+import { NavLink } from "react-router-dom";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 function Settings() {
   return (
     <div className="Settings">
-        <h1>Settings</h1>
-        <NavLink to="/hub">Go to Hub</NavLink>
+      <h1>Settings</h1>
+      <NavLink to="/hub">Go to Hub</NavLink>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+        <Tab eventKey="home" title="Home">
+          Tab content for Home
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          Tab content for Profile
+        </Tab>
+        <Tab eventKey="contact" title="Contact" disabled>
+          Tab content for Contact
+        </Tab>
+      </Tabs>
     </div>
   );
 }
